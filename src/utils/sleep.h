5b0e519ec3e1c8a7bd3bc41492dd672eb92fe589
Szczepan Zalega <szczepan@nitrokey.com>
2016-09-12 20:04:55 +0200

    Revert "Turbo mode (set sleep fixed to 100, ignore sleep parameter)"
    This reverts commit fea4a501805b486824698b387b62a51a84e14f25.

--- src/utils/sleep.h
+++ src/utils/sleep.h
@@ -26,7 +26,7 @@
 class Sleep : public QThread {
 public:
   static void sleep(unsigned long secs) { QThread::sleep(secs); }
-  static void msleep(unsigned long msecs) { QThread::msleep(100); }
+  static void msleep(unsigned long msecs) { QThread::msleep(msecs); }
   static void usleep(unsigned long usecs) { QThread::usleep(usecs); }
 };
 
